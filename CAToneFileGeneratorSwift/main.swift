4078e65df9c2a807bce420c1344d06de6e6e3a9e
Max Harris <harris.max@gmail.com>
2020-11-04 19:46:12 -0800

    fix square wave, add saw and sin wave

--- CAToneFileGeneratorSwift/main.swift
+++ CAToneFileGeneratorSwift/main.swift
@@ -5,6 +5,8 @@
 //  Created by Max Harris on 10/30/20.
 //
 
+// xxd --seek +0x00000ff0 880.000-square.aif | less
+
 import Foundation
 
 import AudioToolbox
@@ -14,7 +16,7 @@ let SAMPLE_RATE: Float64 = 44100
 let DURATION = 5.0
 let FILENAME_FORMAT = "square.aif"
 
-if (CommandLine.arguments.count != 2) {
+if CommandLine.arguments.count != 2 {
     print("Usage: CAToneFileGenerator \n\n(where n is tone in Hz)")
     exit(0)
 }
@@ -24,7 +26,7 @@ print("generating \(hz) Hz tone")
 
 let fileName = "/\(String(describing: hz))-\(FILENAME_FORMAT)"
 let filePath = FileManager.default.currentDirectoryPath + fileName
-let fileURL = NSURL.init(fileURLWithPath: filePath)
+let fileURL = NSURL(fileURLWithPath: filePath)
 
 var asbd = AudioStreamBasicDescription(
     mSampleRate: SAMPLE_RATE,
@@ -35,10 +37,10 @@ var asbd = AudioStreamBasicDescription(
     mBytesPerFrame: 2,
     mChannelsPerFrame: 1,
     mBitsPerChannel: 16,
-    mReserved: 0 // Apple says this must be set to 0
-);
+    mReserved: 0 // tweak for alignment to 8-bit bytes. always set this to 0
+)
 
-var audioFile: AudioFileID? = nil
+var audioFile: AudioFileID?
 var audioErr: OSStatus? = OSStatus(noErr)
 audioErr = AudioFileCreateWithURL(fileURL, UInt32(kAudioFileAIFFType), &asbd, AudioFileFlags.eraseFile, &audioFile)
 
@@ -47,20 +49,19 @@ let maxSampleCount = SAMPLE_RATE
 var sampleCount: CLong = 0
 var bytesToWrite: UInt32 = 2
 let wavelengthInSamples = SAMPLE_RATE / hz
-while (sampleCount < Int(maxSampleCount)) {
-    for i in 0 ... Int(wavelengthInSamples) {
-        // Square wave
-        // UInt16.max = (2 ** bitWidth) - 1 = (2^16) - 1 = 65535
-        // (UInt16.max + 1) / 2 = 32768 = 0x8000
-        // (UInt16.max + 1) / 2 = 32768 - 1 = 0x7fff
-        var sample: UInt16
-        if (i < Int(wavelengthInSamples / 2) + 1) {
-            sample = CFSwapInt16HostToBig(32767)
-        } else {
-            sample = CFSwapInt16HostToBig(32768)
-        }
 
-        audioErr = AudioFileWriteBytes(audioFile!, false, Int64(sampleCount*2), &bytesToWrite, &sample)
+while sampleCount < Int(maxSampleCount) {
+    for i in 0 ... Int(wavelengthInSamples) {
+        // Square Wave
+        // var sample: Int16 = (i < Int(wavelengthInSamples) / 2 ? Int16.max : Int16.min).byteSwapped
+
+        // Saw Wave
+        // var sample = Int16(((Double(i) / wavelengthInSamples) * Double(Int16.max) * 2) - Double(Int16.max)).byteSwapped
+
+        // Sin Wave
+        var sample = Int16(Double(Int16.max) * sin(2 * Double.pi * (Double(i) / wavelengthInSamples))).byteSwapped
+
+        audioErr = AudioFileWriteBytes(audioFile!, false, Int64(sampleCount * 2), &bytesToWrite, &sample)
         sampleCount += 1
     }
 }
